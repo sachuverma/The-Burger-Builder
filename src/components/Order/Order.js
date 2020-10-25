425d00a534bbe17b52fdd412108f9546855ea603
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-25 16:09:51 +0530

    past orders list

---
+++ src/components/Order/Order.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => {
+  const ingredients = [];
+  for(let ingredientName in props.ingredients){
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientList = ingredients.map(ig => {
+    return <span 
+      style={{
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0px 8px',
+        border: '2px solid #bbb',
+        padding: '5px'
+      }}
+      key={ig.name}>
+        {ig.name} ({ig.amount}) 
+      </span>;
+  })
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientList} </p>
+      <p>Price: <strong>$ {Number.parseFloat(props.price.toFixed(2))}</strong></p>
+    </div>
+  );
+};
+
+export default order;
